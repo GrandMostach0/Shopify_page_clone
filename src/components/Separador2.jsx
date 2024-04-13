b80cc728a4786cbf1efe62a35b2e9f43b1525238
grandmostach0 <kreedlegend0@gmail.com>
2024-04-13 17:34:22 -0600

    agregando algunas imagnes de los sponsors, algunos faltaron

--- src/components/Separador2.jsx
+++ src/components/Separador2.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './style/containerS.css'
+import LogoMattel from '../assets/Sponsors/LogoMattel.svg';
+import LogoGlossier from '../assets/Sponsors/LogoGlossier.svg';
+import LogoStaples from '../assets/Sponsors/LogoStaples.svg';
+import LogoCoty from '../assets/Sponsors/LogoCoty.svg';
 
 function Separador2() {
   return (
     <div className="containerS">
       <div className="container-images">
+        <img src={LogoMattel} alt="" />
         <p>imagen</p>
+        <img src={LogoGlossier} alt="" />
         <p>imagen</p>
-        <p>imagen</p>
-        <p>imagen</p>
-        <p>imagen</p>
-        <p>imagen</p>
+        <img src={LogoStaples} alt="" />
+        <img src={LogoCoty} alt="" />
         <p>imagen</p>
       </div>
     </div>
