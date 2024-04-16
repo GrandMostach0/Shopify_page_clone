8edad52d1f27baee6c289b3564cfc60ee85e2f1c
grandmostach0 <kreedlegend0@gmail.com>
2024-04-16 11:40:53 -0600

    agregando font y detalles del separador para mostrar a los sponsors

--- src/components/Separador.jsx
+++ src/components/Separador.jsx
@@ -3,28 +3,33 @@ import imagenSeparador from '../assets/images/hero-Desktop-separator1.png';
 import FlechaRight from './icons/row_direction_right';
 import './style/separador.css';
 
-function Separador(){
-    return (
-      <div className="separadorContainer">
+function Separador() {
+  return (
+    <div className="separadorContainer">
+      <div className="separadorContainerText">
         <div className="textoSeparador">
-          <h1 className="separador-title">Build any commerce experience</h1>
-          <hr />
-          <p>
-            Compose, customize, and extend every part of the commerce stack—from
-            storefront to checkout to backend integrations—and create unique
-            experiences for your brand or millions of merchants around the
-            world.
-          </p>
-          <div className="ancors">
-            <a href="#">
-              Explore docs
-            </a>
-            <FlechaRight />
+          <div className="separadorText">
+            <h1 className="separador-title">Build any commerce experience</h1>
+            <hr />
+            <p>
+              Compose, customize, and extend every part of the commerce
+              stack—from storefront to checkout to backend integrations—and
+              create unique experiences for your brand or millions of merchants
+              around the world.
+            </p>
+            <div className="ancors">
+              <a href="#">Explore docs</a>
+              <FlechaRight />
+            </div>
+          </div>
+          <div className="imagen-container">
+            <img className="imagenSeparador" src={imagenSeparador} alt="" />
           </div>
         </div>
-        <img src={imagenSeparador} alt="" />
       </div>
-    );
+    </div>
+  );
 }
+
 
 export default Separador;
