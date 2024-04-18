c308b73867eb18c11c63595b7cd50d967c269f8c
grandmostach0 <kreedlegend0@gmail.com>
2024-04-17 22:01:49 -0600

    terminando la seccion de los benenficios

--- src/components/Separador3.jsx
+++ src/components/Separador3.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Backgrund from '../assets/images/home-apis-light-.webp';
 import './style/separador.css';
 
 function Separador3(){
@@ -11,58 +12,81 @@ function Separador3(){
               commercer
             </h1>
             <hr />
-            <div className="separador-left">.
-              <p>
-                Shopify’s platform provides APIs, SDKs, primitives, and tools
-                for building storefronts, custom cart and checkout flows, and
-                seamless integrations with your systems.
-              </p>
-              <p>
-                Get full flexibility to assemble, customize, and extend the
-                entire commerce experience, all with the backing of our global
-                infrastructure, scalability, and reliability.
-              </p>
-            </div>
+            <div className="container-separador">
+              <div className="separador-left">
+                <p>
+                  Shopify’s platform provides APIs, SDKs, primitives, and tools
+                  for building{" "}
+                  <a href="#" className="ancors-span">
+                    storefronts
+                  </a>
+                  , custom cart and{" "}
+                  <a href="#" className="ancors-span">
+                    checkout
+                  </a>{" "}
+                  flows, and seamless{" "}
+                  <a href="#" className="ancors-span">
+                    integrations
+                  </a>{" "}
+                  with your systems.
+                </p>
+                <p>
+                  Get full flexibility to assemble, customize, and extend the
+                  entire commerce experience, all with the backing of our global
+                  infrastructure, scalability, and reliability.
+                </p>
+              </div>
 
-            <div className="separador-right">
-              <div className="separador-right-tags">
-                <p>APIs in GraphQL and REST</p>
-              </div>
-              <div className="separador-right-tags">
-                <p>
-                  React-based framework and component library for accelerating
-                  storefront development
-                </p>
-              </div>
-              <div className="separador-right-tags">
-                <p>
-                  React-based framework and component library for accelerating
-                  storefront development
-                </p>
-              </div>
-              <div className="separador-right-tags">
-                <p>
-                  Commerce primitives for building subscription plans, selling
-                  B2B, expanding internationally, and more
-                </p>
-              </div>
-              <div className="separador-right-tags">
-                <p>
-                  Shopify CLI for building, testing, and managing your
-                  integration
-                </p>
-              </div>
-              <div className="separador-right-tags">
-                <p>Ready-to-use UI components for getting started quickly</p>
-              </div>
-              <div className="separador-right-tags">
-                <p>
-                  Ecosystem of prebuilt and reference implementations for
-                  integrations with systems like Netsuite and Sanity
-                </p>
+              <div className="separador-right">
+                <div className="separador-right-tags">
+                  <div className="check">✅</div>
+                  <p>APIs in GraphQL and REST</p>
+                </div>
+                <div className="separador-right-tags">
+                  <div className="check">✅</div>
+                  <p>
+                    React-based framework and component library for accelerating
+                    storefront development
+                  </p>
+                </div>
+                <div className="separador-right-tags">
+                  <div className="check">✅</div>
+                  <p>
+                    React-based framework and component library for accelerating
+                    storefront development
+                  </p>
+                </div>
+                <div className="separador-right-tags">
+                  <div className="check">✅</div>
+                  <p>
+                    Commerce primitives for building subscription plans, selling
+                    B2B, expanding internationally, and more
+                  </p>
+                </div>
+                <div className="separador-right-tags">
+                  <div className="check">✅</div>
+                  <p>
+                    Shopify CLI for building, testing, and managing your
+                    integration
+                  </p>
+                </div>
+                <div className="separador-right-tags">
+                  <div className="check">✅</div>
+                  <p>Ready-to-use UI components for getting started quickly</p>
+                </div>
+                <div className="separador-right-tags">
+                  <div className="check">✅</div>
+                  <p>
+                    Ecosystem of prebuilt and reference implementations for
+                    integrations with systems like Netsuite and Sanity
+                  </p>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className="container-imagen-separador">
+          <img src={Backgrund} alt="" />
         </div>
       </div>
     );
